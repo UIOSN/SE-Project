f24f2b1812c3fabb656a0f8f4193ff4ce09c2a07
mmrrb <2858745049@qq.com>
2025-04-27 18:15:23 +0800

    rebuild

--- Vue-app/vite.config.js
+++ Vue-app/vite.config.js
@@ -4,4 +4,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: '/SE-Project/Vue-app', // 必须与GitHub仓库名完全一致
+  build: {
+    outDir: '../../docs/Vue-app',
+  }
 })
