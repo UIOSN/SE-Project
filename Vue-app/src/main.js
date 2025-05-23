e0d9dba76dd8bd5e058c27e3ab669579408b97aa
pan <2225874433@qq.com>
2025-05-23 12:52:14 +0800

    delete old Vue-app

--- Vue-app/src/main.js
+++ Vue-app/src/main.js
@@ -1,10 +1,26 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router' // 自动识别index.js
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-// 如果不需要CSS可以先注释掉
-// import './assets/main.css'
+import Aura from '@primeuix/themes/aura';
+import PrimeVue from 'primevue/config';
+import ConfirmationService from 'primevue/confirmationservice';
+import ToastService from 'primevue/toastservice';
 
-createApp(App)
-  .use(router)
-  .mount('#app')
+import '@/assets/styles.scss';
+
+const app = createApp(App);
+
+app.use(router);
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: '.app-dark'
+        }
+    }
+});
+app.use(ToastService);
+app.use(ConfirmationService);
+
+app.mount('#app');
